3a8de5296412a37839ae409cd502d0d0f127147b
BEATRIZ RODRIGUES DE SOUZA <23111776@dsn127m1242374.cfp127aluno.local>
2025-08-07 17:21:37 -0300

    falta subir pro render

---
+++ front-end/api.js
@@ -0,0 +1,12 @@
+// api.js
+const API_URL = "http://localhost:3000/fotos"
+
+export async function buscarFotos() {
+    try {
+        const resposta = await fetch(API_URL)
+        const dados = await resposta.json()
+        return dados
+    } catch (erro) {
+        return []
+    }
+}
